6b54288239c61e21e5274654a0c5e5055302ade7
bryanl <bryan@osesm.com>
2010-04-27 14:38:40 -0400

    updating app

--- sig_app.rb
+++ sig_app.rb
@@ -21,7 +21,7 @@ post '/' do
   cache = Memcached.new
   key = (0...8).map{65.+(rand(25)).chr}.join
   cache.set key, @path
-  erb "<img src='/signature.png?key=#{key}'><p><a href='/'>start over</a><p>#{@path}</p>"
+  erb "<img src='/signature.png?key=#{key}'><p><a href='/'>start over</a>"
 end
 
 get '/signature.png' do
@@ -33,13 +33,13 @@ get '/signature.png' do
   canvas = Magick::Image.new(500, 200)
   gc = Magick::Draw.new
   gc.stroke('black')
-  gc.stroke_width(5)
+  gc.stroke_width(1)
 
   path = cache.get params[:key]
   path.split(/ /).each do |stroke|
-    origin = stroke
-    line = stroke.gsub('M', 'L')
-    gc.path [origin, line].join(" ")
+    stroke.scan(%r{^M(\d+,\d+),(\d+,\d+)$}) {
+      gc.path("M#{$1} L#{$2}")
+    }
   end
 
   gc.draw canvas
